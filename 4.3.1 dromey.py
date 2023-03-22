8a41443b3dbc527eff6f02df12ecbac2cd8d7216
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-03-22 12:51:14 +0530

    Add files via upload

---
+++ 4.3.1 dromey.py
@@ -0,0 +1,6 @@
+marks = [78, 90, 85, 65, 70, 80, 92, 88, 75, 83, 72, 68, 90, 95, 87, 80, 85, 92, 76, 85]
+max =  marks[0]
+for num in marks:
+    if num>max:
+        max = num
+print("maximum value: ",max)   
